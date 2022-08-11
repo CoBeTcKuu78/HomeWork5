4423d01c4f8911f18b687be7450345c55ea8b018
CoBeTcKuu <9437855@gmail.com>
2022-08-11 22:39:29 +0300

    1 задание

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int [] arr = new int [3];
+        arr [0] = 1;
+        arr [1] = 2;
+        arr [2] = 3;
+        //int [] arr = {1, 2, 3};
+        //System.out.println(arr);
+        float [] drobChisla = {-1.57f, 7.654f, 9.986f};
+
+        int [] array = new int [10];
+        int arrSise = array.length;
+        System.out.println(arrSise);
+
+
     }
 }
